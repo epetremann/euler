e805b096dcb3287c223f2f024352dd83a8954919
epetremann <epetremann@gmail.com>
2020-11-23 19:02:24 +0900

    solved problem 61

--- lib/problem61.ex
+++ lib/problem61.ex
@@ -28,99 +28,75 @@ defmodule Problem61 do
   @doc """
   
   """
-  def solve() do
-    for a <- 10..99, 
-      b <- Enum.to_list(10..99) -- [a],
-      c <- Enum.to_list(10..99) -- [a,b],
-      d <- Enum.to_list(10..99) -- [a,b,c],
-      e <- Enum.to_list(10..99) -- [a,b,d],
-      f <- Enum.to_list(10..99) -- [a,b,e]
-    do
-      cyclic_list = [
-        a*100 + b,
-        b*100 + c,
-        c*100 + d,
-        d*100 + e,
-        e*100 + f,
-        f*100 + a
-      ]
-      p3_list = 
-        cyclic_list 
-        |> Enum.filter(fn x -> find_pn(x,&p3/1) end)
-      if length(p3_list) > 0 do
-        p4_list = 
-          (cyclic_list -- p3_list)
-          |> Enum.filter(fn x -> find_pn(x,&p4/1) end)
-        if length(p4_list) > 0 do
-          p5_list = 
-          (cyclic_list -- p3_list -- p4_list)
-          |> Enum.filter(fn x -> find_pn(x,&p5/1) end)
-          if length(p5_list) > 0 do
-            p6_list =
-              (cyclic_list -- p3_list -- p4_list -- p5_list)
-              |> Enum.filter(fn x -> find_pn(x,&p6/1) end)
-            if length(p6_list) > 0 do
-              p7_list =
-                (cyclic_list -- p3_list -- p4_list -- p5_list -- p6_list)
-                |> Enum.filter(fn x -> find_pn(x,&p7/1) end)
-              if length(p7_list) > 0 do
-                p8_list =
-                  (cyclic_list -- p3_list -- p4_list -- p5_list -- p6_list -- p7_list)
-                  |> Enum.filter(fn x -> find_pn(x,&p8/1) end)
-                if length(p8_list) > 0 do
-                  IO.inspect cyclic_list
-                end #p8
-              end #p7
-            end #p6
-          end #p5
-        end #p4
-      end #p3
+  def solve(n1 \\ 1000, n2 \\ 9999) do
+    plist3 = get_pnlist(n1, n2, &p3/1)
+    plist4 = get_pnlist(n1, n2, &p4/1)
+    plist5 = get_pnlist(n1, n2, &p5/1)
+    plist6 = get_pnlist(n1, n2, &p6/1)
+    plist7 = get_pnlist(n1, n2, &p7/1)
+    plist8 = get_pnlist(n1, n2, &p8/1)
+
+    all_numbers_list = [ plist3, plist4, plist5, plist6, plist7, plist8 ]
+    for l1 <- [ plist3, plist4, plist5, plist6, plist7, plist8 ] do
+      for l2 <- (all_numbers_list -- [l1]) do
+        for l3 <- (all_numbers_list -- [l1, l2]) do
+          for l4 <- (all_numbers_list -- [l1, l2, l3]) do
+            for l5 <- (all_numbers_list -- [l1, l2, l3, l4]) do
+              for l6 <- (all_numbers_list -- [l1, l2, l3, l4, l5]) do
+                    get_suites(l1, l2, l3, l4, l5, l6 )
+              end
+            end
+          end
+        end
+      end
     end
-    |> Enum.filter(&(&1))
+    |> List.flatten
   end
 
   @doc """
-  Triangle 	  	P3,n=n(n+1)/2 	  	1, 3, 6, 10, 15, ...
+  generates all 4-digits suites from two lists
+  """
+  def get_suites(l1, l2, l3, l4, l5, l6) do
+      for px1 <- l1,
+        px2 <- (l2 |> Enum.filter(fn x -> rem(px1,100) == div(x,100) end)), 
+        px3 <- (l3 |> Enum.filter(fn x -> rem(px2,100) == div(x,100) end)), 
+        px4 <- (l4 |> Enum.filter(fn x -> rem(px3,100) == div(x,100) end)), 
+        px5 <- (l5 |> Enum.filter(fn x -> rem(px4,100) == div(x,100) end)), 
+        px6 <- (l6 |> Enum.filter(fn x -> rem(px5,100) == div(x,100) && rem(x,100) == div(px1,100)end)) do 
+          { Enum.sum([px1, px2, px3, px4, px5, px6]), [px1, px2, px3, px4, px5, px6]}
+    end 
+  end
+
+
+  @doc """
+  Triangle 	 	P3,n=n(n+1)/2 	 	1, 3,  6, 10, 15, ...
+  Square 	  	P4,n=n2 	  	    1, 4,  9, 16, 25, ...
+  Pentagonal 	P5,n=n(3n−1)/2 	 	1, 5, 12, 22, 35, ...
+  Hexagonal  	P6,n=n(2n−1) 	  	1, 6, 15, 28, 45, ...
+  Heptagonal 	P7,n=n(5n−3)/2 	 	1, 7, 18, 34, 55, ...
+  Octagonal  	P8,n=n(3n−2) 	  	1, 8, 21, 40, 65, ...
   """
   def p3(n), do: n*(n+1) |> div(2)
-
-  @doc """
-  Square 	  	P4,n=n2 	  	1, 4, 9, 16, 25, ...
-  """
   def p4(n), do: n*n
-
-  @doc """
-  Pentagonal 	  	P5,n=n(3n−1)/2 	  	1, 5, 12, 22, 35, ...
-  """
   def p5(n), do: n*(3*n-1) |> div(2)
-  
-  @doc """
-  Hexagonal 	  	P6,n=n(2n−1) 	  	1, 6, 15, 28, 45, ...
-  """
-  def p6(n), do:  n*(2*n-1)
-
-  @doc """
-  Heptagonal 	  	P7,n=n(5n−3)/2 	  	1, 7, 18, 34, 55, ...
-  """
+  def p6(n), do: n*(2*n-1)
   def p7(n), do: n*(5*n-3) |> div(2)
-
-  @doc """
-  Octagonal 	  	P8,n=n(3n−2) 	  	1, 8, 21, 40, 65, ...
-  """
   def p8(n), do: n*(3*n-2)
 
+
+
   @doc """
-  is_pn?(&pn/1, n) searches for the smallest number x : pn(x) = n
-  where pn is one of [p3, p4, p5, p6, p7, p8]
-  if there is no such number, returns nil
+  lis_pn(n1,n2, pn) generates a list of all pn (triangle, square etc.) numbers comprised between
+  n1 and n2 : n1 <= pn(n) <= n1.
   """
-  def find_pn(n, pn), do: do_find_pn(n, pn, 1) 
-  def do_find_pn(n, pn, i) do
+  def get_pnlist(n1, n2, func_pn), do: do_get_pnlist(n1, n2, func_pn, 1, [])
+  def do_get_pnlist(n1, n2, func_pn, i, pnlist) do
+    pni = func_pn.(i)
     cond do
-      pn.(i) > n -> nil
-      pn.(i) == n -> i
-      pn.(i) < n -> do_find_pn(n, pn, i+1)
+      pni > n2 -> pnlist
+      pni < n1 -> do_get_pnlist(n1, n2, func_pn, i+1, pnlist)
+      pni >= n1 and 
+      pni <= n2 -> do_get_pnlist(n1, n2, func_pn, i+1, [pni | pnlist])
     end
   end
 end
-
